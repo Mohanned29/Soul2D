a034d2497887b43b0bfb14c5d6c890cfb9de1f82
jinxIsOp <154748750+jinxIsOp@users.noreply.github.com>
2024-04-09 22:02:48 +0100

    Heart System Class

---
+++ ui_elements.py
@@ -0,0 +1,8 @@
+import pygame
+#Heart System Class
+class Heart(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.image = pygame.image.load('graphics/heart.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (40, 40))  # Adjust size as needed
+        self.rect = self.image.get_rect(topleft=pos)
